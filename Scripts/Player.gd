798c62df5f0658fd72c6a470274aabe0a3d9bc74
FIIZYLemon <matherdylan04@gmail.com>
2024-07-23 20:07:20 +0100

    Player Linecast added
    The visible line checker has been added to allow the player to see whether an object is grabbable or not

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,15 @@ var jumpBuffer:float = 0.0
 @export_range(0.0,1.0)var acceleration=0.25
 @export_range(0.0,1.0)var friction=0.1
 var direction = 0
+var end_point:Vector2 = Vector2.ZERO
 
+var line_colour:Color = Color(255,0,0,0.5)
 		
+func _draw():
+	if Input.is_action_pressed("Lick"):
+		draw_line(to_local(global_position),to_local(end_point),line_colour,3,true)
 	
+		
 func _physics_process(delta):
 	
 	if jumpBuffer > 0.0:
@@ -29,3 +35,18 @@ func _physics_process(delta):
 		
 	if jumpBuffer>0.0&&is_on_floor():
 		velocity.y=-jump_height
+		
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position,get_global_mouse_position(),collision_mask,[self])
+	query.collide_with_areas=true
+	var result = space_state.intersect_ray(query)
+	if result:
+		end_point=result.position
+		if result.collider.is_in_group('Lickable'):
+			line_colour = Color(0,255,0,0.5)
+		else:
+			line_colour=Color(255,0,0,0.5)
+	else:
+		line_colour=Color(255,0,0,0.5)
+		end_point=get_global_mouse_position()
+	queue_redraw()
