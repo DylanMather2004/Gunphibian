d16fd6fed524fe37e5e6ffb0756070109435b13a
FIIZYLemon <matherdylan04@gmail.com>
2024-07-26 17:08:47 +0100

    Fling mechanic added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 @export var move_speed = 400
 @export var jump_height = 400
 @export var gravity = 3000
+@export var fling_force = 500
 var jumpBuffer:float = 0.0
 @export_range(0.0,1.0)var acceleration=0.25
 @export_range(0.0,1.0)var friction=0.1
 var direction = 0
 var end_point:Vector2 = Vector2.ZERO
+var target:Node2D=null
+
 
 var line_colour:Color = Color(255,0,0,0.5)
 		
@@ -28,6 +31,9 @@ func _physics_process(delta):
 		velocity.x=lerp(velocity.x,0.0,friction)
 	velocity.y+=gravity*delta
 	
+	if Input.is_action_just_released("Lick"):
+		_fling_calculation()
+	
 	move_and_slide()
 	
 	if Input.is_action_just_pressed('Jump'):
@@ -44,9 +50,24 @@ func _physics_process(delta):
 		end_point=result.position
 		if result.collider.is_in_group('Lickable'):
 			line_colour = Color(0,255,0,0.5)
+			if Input.is_action_pressed("Lick"):
+				target=result.collider
+			
 		else:
 			line_colour=Color(255,0,0,0.5)
+			target=null
 	else:
 		line_colour=Color(255,0,0,0.5)
 		end_point=get_global_mouse_position()
 	queue_redraw()
+	
+	
+func _fling_calculation():
+	if target !=null:
+		var flingdir = (target.position-position).normalized()
+		var flingvelocity =flingdir*fling_force
+		
+		velocity=flingvelocity
+		
+		
+		
