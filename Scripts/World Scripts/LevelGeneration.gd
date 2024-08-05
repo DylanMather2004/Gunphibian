9e9dac4d3d978fe4d0139c7626bd38c1c275a837
FIIZYLemon <matherdylan04@gmail.com>
2024-08-05 14:30:49 +0100

    End room created for the level generator
    Reworks to the generation script. Spawn function now takes a PackedScene as an argument instead of an integer, and selection from the rooms array now takes palace outside the function

--- Scripts/World Scripts/LevelGeneration.gd
+++ Scripts/World Scripts/LevelGeneration.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
 @export var Rooms = []
+@export var EndRoom:PackedScene
 @export var CurrentRoom:Node2D
 @export var number_of_rooms = 10
 var rng = RandomNumberGenerator.new()
 
-func _spawn(roomID):
-	var room_to_spawn:Node2D = Rooms[roomID].instantiate()
+func _spawn(roomID:PackedScene):
+	var room_to_spawn:Node2D = roomID.instantiate()
 	room_to_spawn.position = CurrentRoom.get_child(0).get_global_position()
 	add_child(room_to_spawn)
 	CurrentRoom=room_to_spawn
 func _ready():
 	for n in number_of_rooms:
 		rng.randomize()
-		var roomID=rng.randi_range(0,Rooms.size()-1)
-		_spawn(roomID)
+		_spawn(Rooms[rng.randi_range(0,Rooms.size()-1)])
+	_spawn(EndRoom)
