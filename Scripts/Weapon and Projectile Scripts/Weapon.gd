8c5f9565936fd2b9c2a526a6f8df2a1f69829506
FIIZYLemon <matherdylan04@gmail.com>
2024-09-12 13:13:30 +0100

    Bullet Spread Implemented
    -----Changelog-----
    - Bullet Spread added to base weapon class
    - Input function code added to seperate function to allow for easier use with derivative classes

--- Scripts/Weapon and Projectile Scripts/Weapon.gd
+++ Scripts/Weapon and Projectile Scripts/Weapon.gd
@@ -1,12 +1,32 @@
+class_name Weapon
 extends Node2D
 
 @export var bullet_pref : PackedScene
+@export var damage=2
 @export var bullet_velocity:float=100
+@export var equipped = false
+var rng = RandomNumberGenerator.new()
+var can_fire
+var fire_rate = 0.2
+@export var bullet_spread = 0.1
+func _ready():
+	$Sprite.visible = equipped
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
+	if equipped:
+		look_at(get_global_mouse_position())
 	
 func _input(event):
-	if Input.is_action_just_pressed('shoot'):
-		var bullet_instance=bullet_pref.instantiate()
-		owner.owner.add_child(bullet_instance)
-		bullet_instance.transform=$Firepoint.global_transform
+	if equipped:
+		if Input.is_action_just_pressed('shoot'):
+			_Shoot()
+
+func _Equip():
+	equipped = not equipped
+	$Sprite.visible = equipped
+func _Shoot():
+	var bullet_instance:Node2D=bullet_pref.instantiate()
+	owner.owner.add_child(bullet_instance)
+	bullet_instance.transform=$Firepoint.global_transform
+	rng.randomize()
+	bullet_instance.rotation += rng.randf_range(-bullet_spread,bullet_spread)
+	
